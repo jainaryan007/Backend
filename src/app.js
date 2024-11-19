9dac87f64705b6532c68cfa3faa112ae52b0421f
jainaryan007 <154777132+jainaryan007@users.noreply.github.com>
2024-11-19 20:03:28 +0530

    basic setup configured with routes and controllers

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express"
 import cookieParser from "cookie-parser";  
 import cors  from "cors"
 
+
 const app = express();
 
 app.use(cors(
@@ -15,5 +16,18 @@ app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true , limit: "16kb"}))  //extended is basically for that ki aap objects ke andar objects le skte ho
 app.use(express.static("public")) //public assets store krne ke liye public folder mai 
 app.use(cookieParser())  //mai server se jo user ka browser h uski cookies access bhhi krr pau aur change bhi kar pau
+
+
+
+//routes import
+import userRouter from './routes/user.routes.js'
+
+
+//routes declaration
+app.use("/api/v1/users", userRouter);
+
+app.get("/test", (req, res) => {
+    res.status(200).json({ message: "Test route works!" });
+});
 
 export { app }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 // require('dotenv').config({path: './env'})
 import dotenv from "dotenv"
 import connectDB from "./db/index.js"
+
+import {app} from './app.js'
+ 
 dotenv.config({
     path: './.env'
 })
+
+
 
 connectDB()
 .then(() => {
